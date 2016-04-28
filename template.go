bce4ca731018d0634d203c92a1295fd9a9b1cdd8
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2016-04-28 15:54:56 +0600

    template: rename func

--- template.go
+++ template.go
@@ -1,15 +1,17 @@
 package main
 
-import "bufio"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"text/template"
+)
 
-import "fmt"
-
-import "os"
-import "text/template"
-
-func getHookTemplateFunctions(vars map[string]string) template.FuncMap {
+func getHookTemplateFunctions(
+	vars map[string]string,
+) template.FuncMap {
 	return template.FuncMap{
-		"_": func(name string) string {
+		"var": func(name string) string {
 			value, ok := vars[name]
 			if ok {
 				return value
